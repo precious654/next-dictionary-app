85b1383895006e9820c64a17502272fc826623f1
Odukwe Precious <odukwechiemeka@gmail.com>
2023-09-30 18:45:43 +0100

    finished the application

--- src/components/Meaning.tsx
+++ src/components/Meaning.tsx
@@ -5,11 +5,17 @@ import {motion} from 'framer-motion'
 import Word from '@/components/Word'
 import { useAppSelector } from '@/redux/store'
 import styles from "./meaning.module.css"
+import { Poppins } from 'next/font/google';
 
+const poppins = Poppins({
+  weight: ["400"],
+  subsets: ["latin"]
+});
 
 export default function Meaning() {
 
   const word = useAppSelector((state) => state.wordReducer.value.word);
+  console.log( word );
 
   const playAudio = () => {
     const audio = word[0].phonetics.filter( (item: any) => item.audio !== "");
@@ -24,13 +30,28 @@ export default function Meaning() {
         initial={{y: 60, opacity: 0}}
         animate={{y: 20, opacity: 1}}
         transition={{ ease: "linear",duration: 1}}
-        className={styles.word}
+        className={poppins.className}
         >
-          <Word word={word[0].word} phonetics={word[0].phonetic}/>
+          <div className={styles.word}>
+            <Word word={word[0].word} phonetics={word[0].phonetic}/>
+            <button onClick={playAudio}>
+              <Icon icon="bi:play-circle-fill" color="#f2f" height={30} />
+            </button>
+          </div>
 
-          <button onClick={playAudio}>
-            <Icon icon="bi:play-circle-fill" color="#f2f" height={30} />
-          </button>
+          <div className={styles.definitions}>
+            {word[0].meanings.map((item:any) => (
+                <div>
+                  <h3>{item.partOfSpeech}</h3>
+
+                  <ul>
+                    {item.definitions.map( (def: any) => (
+                      <li>{def.definition}</li>
+                    ))}
+                  </ul>
+                </div>
+              ))}
+          </div>
         </motion.div>
       }
     </>
